3121d86044f1351dbcddc2954fc610a90c006f3b
Welcome <Welcome@DESKTOP-99COHO2>
2022-01-25 18:40:09 +0530

    Employee business logic and Assert Types

--- JUnitFramework/src/test/java/junitPrograms/CallingMethods.java
+++ JUnitFramework/src/test/java/junitPrograms/CallingMethods.java
@@ -8,20 +8,21 @@ public class CallingMethods {
 	public void testCallingMethod1() {
 		System.out.println("I am test calling method1 from Class: CallingMethods");
 	}
-	
+
 	@Test
 	public void testCallingMethod2() {
-		System.out.println("I am test calling method2 from Class: "+getClass());
-		// calling another class test method by using Class name directly only if it is STATIC
+		System.out.println("I am test calling method2 from Class: " + getClass());
+		// calling another class test method by using Class name directly only if it is
+		// STATIC
 		ExeOrderAnnotations.beforeClassMethod();
 		ExeOrderAnnotations.afterclassMethod();
 	}
-	
+
 	@Test
 	public void testCallingMethod3() {
 		System.out.println("I am test calling method3 from Class: CallingMethods");
-		// calling another class method by creating object, even it is non-static 
-		ExeOrderAnnotations eoa=new ExeOrderAnnotations();
+		// calling another class method by creating object, even it is non-static
+		ExeOrderAnnotations eoa = new ExeOrderAnnotations();
 		eoa.testMethod1();
 		eoa.testMethod2();
 	}
